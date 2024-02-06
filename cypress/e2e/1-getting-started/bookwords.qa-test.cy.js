fb4f32ad94156f6e7f7fc5cb3e347b71525487bf
Jianying Wu <fishinfall@126.com>
2024-02-05 16:33:51 -0800

    1.1.1 try a incorrect link with failure

--- cypress/e2e/1-getting-started/bookwords.qa-test.cy.js
+++ cypress/e2e/1-getting-started/bookwords.qa-test.cy.js
@@ -17,6 +17,6 @@ describe('Bookwords QA test', () => {
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit('https://qa.bookwords.cn')
+    cy.visit('https://qa1.bookwords.cn')
   })
 })
